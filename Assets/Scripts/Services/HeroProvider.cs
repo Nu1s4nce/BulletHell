2fd5323a65b3405247966dc77abb003a605ee9a8
Nuisance <tixonmakarov2019@yandex.ru>
2024-09-17 19:48:24 +0300

    delete ecs. add services

---
+++ Assets/Scripts/Services/HeroProvider.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class HeroProvider : IHeroProvider
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetHero()
+    {
+        
+    }
+}
